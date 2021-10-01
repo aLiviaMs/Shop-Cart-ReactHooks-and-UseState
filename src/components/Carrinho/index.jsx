f9326ef4352693de5a7ad313132a121df3a7dfcd
Lívia Minucelli <liviawrk@gmail.com>
2021-10-01 19:34:05 -0300

    Finishing simple UseState application.

--- src/components/Carrinho/index.jsx
+++ src/components/Carrinho/index.jsx
@@ -1,13 +1,29 @@
+import { useState } from 'react';
+
 const Carrinho = () => {
-    let carrinho = 0;
+
+    let [count, setCount] = useState(0);
+
+
+    const AddItem = () => {
+        setCount(count + 1);
+    }
+    const RemoveItem = () => {
+        if (count > 0) {
+            setCount(count - 1);
+        }
+    }
+
 
     return (
         <>
             <section className="container">
-                <h1>Carrinho de compras: {carrinho}</h1>
+                <h1>Carrinho de compras: {count}</h1>
 
-                <button className="add-button">+</button>
-                <button className="remove-button">-</button>
+                <div className="flex">
+                    <button className="add-button" onClick={AddItem}>+</button>
+                    <button className="remove-button" onClick={RemoveItem}>-</button>
+                </div>
             </section>
         </>
     )
